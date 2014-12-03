4312a5c47ba972ac7ba495cdc03f1c26b64d9cf2
Asa Toburen <atopettit@gmail.com>
2014-12-03 10:50:09 -0700

    added additional functionality

--- js/app.js
+++ js/app.js
@@ -1,74 +1,115 @@
 
-$( document ).ready(function() {
-	$('#end').hide();
-	$('#question').hide();
-
-
-function Question(prompt, first, second, third, fourth) {
-   this.prompt = prompt;
-   this.answerA = first;
-   this.answerB = second;
-   this.answerC = third;
-   this.answerD = fourth;
-	this.addToQuiz = function() {
-	 		return "<h1>" + this.prompt + "</h1><ol><li>" + this.answerA  + "</li><li>" + this.answerB  + "</li><li>" + this.answerC  + "</li><li>" + this.answerD  + "</li></ol>";
-};
-
-$('button').click(function (question1){
-		$('#btn').hide();
 
 var question1 = {
-	prompt : "What percentage of the earth's water is fresh water?",
-	answerA : "7%",
-	answerB : "12%",
-	answerC : "9%",	
-	answerD : "3%"
+  questionText : "What percentage of the earth's water is fresh water?",
+  answerA : "7%",
+  answerB : "12%",
+  answerC : "9%", 
+  answerD : "3%",
+    correct: 1
 }
 
 var question2 = {
-	prompt : "At 1 drip per second, approximately how much water does a faucet leak per year?</h1>",
-	answerA : "160 gallons",
-	answerB : "3000 gallons",
-	answerC : "225 gallons",
-	answerD : "1500 gallons"
+  questionText: "At 1 drip per second, approximately how much water does a faucet leak per year?",
+  answerA : "160 gallons",
+  answerB : "3000 gallons",
+  answerC : "225 gallons",
+  answerD : "1500 gallons",
 }
 var question3 = {
-	prompt : "On average, what percentage of the human body is made of water?</h1>",
-	answerA : "75%",
-	answerB : "87%",
-	answerC : "60%",
-	answerD : "55%"
+  questionText: "On average, what percentage of the human body is made of water?",
+  answerA : "75%",
+  answerB : "87%",
+  answerC : "60%",
+  answerD : "55%",
 }
 
 var question4 = {
-	prompt : "The Average American uses how many gallons of water per day?</h1>",
-	answerA : "99 gallons",
-	answerB : "44 gallons",
-	answerC : "72 gallons",
-	answerD : "125 gallons"
+  questionText: "The Average American uses how many gallons of water per day?",
+  answerA : "99 gallons",
+  answerB : "44 gallons",
+  answerC : "72 gallons",
+  answerD : "125 gallons",
 }
 
 var question5 = {
-	prompt : "The oceans hold what percentage of the earth's water?",
-	answerA : "96.5%",
-	answerB : "84.7%",
-	answerC : "65.2%",
-	answerD : "86%"
+  questionText: "The oceans hold what percentage of the earth's water?",
+  answerA : "96.5%",
+  answerB : "84.7%",
+  answerC : "65.2%",
+  answerD : "86%",
+
 }
 
-
-//question array to loop through question objects 
-
 var questionArray = [question1, question2, question3, question4, question5];
-for	(i = 0; i < questionArray.length; i++) {
-  text += questionArray[i];
+var correctAnswerArray = [question1.answerA, question2.answerB, question3.answerC, question4.answerA, question5.answerA];
+var userAnswerArray = [];
+var questionNum = 0;
+var userScore = 0;
 
- console.log('Ready');
 
-};
+var questionElement = document.getElementById('questionText');
+var answerEl1 = document.getElementById('a1');
+var answerEl2 = document.getElementById('a2');
+var answerEl3 = document.getElementById('a3');
+var answerEl4 = document.getElementById('a4');
+
+
+
+
+var nextQuestion = function () {  
+  questionElement.innerHTML = questionArray[questionNum].questionText;
+   answerEl1.innerHTML = questionArray[questionNum].answerA;
+   answerEl2.innerHTML = questionArray[questionNum].answerB;
+   answerEl3.innerHTML = questionArray[questionNum].answerC;
+   answerEl4.innerHTML = questionArray[questionNum].answerD;
+    var answerElements = [answerEl1, answerEl2, answerEl3, answerEl4];
+    var answerElementsLength = answerElements.length;
+    
+    console.log(answerElements);
+
+    for (var i = 0; i < answerElementsLength; i++) {
+      answerElements[i].addEventListener('click', bindClick(i));
+}
+
+  function bindClick(i) {
+    return function(){
+      userAnswerArray.push(this.innerHTML);
+        	preventDefault();
+      var userAnswer = this.innerHTML;
+      console.log(userAnswerArray);
+      console.log(correctAnswerArray);
+
+  if (userAnswer == correctAnswerArray[questionNum]) {
+      userScore++;
+      //alert user
+    }
+    questionNum++;
+    nextQuestion();
+
+  };
+  };
+  };
+
+//starts quiz after button is clicked//
+document.getElementById('btn').addEventListener("click", function(){
+  document.getElementById('btn').remove();
+  nextQuestion();
 });
-};
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
